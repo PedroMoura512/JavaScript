06028ca532fd27e23df5bb8ad029a750a274c078
Pedro Moura <pmouramiguelmoura@gmail.com>
2025-04-04 18:22:26 -0300

    aprendendo funções

---
+++ funções.js
@@ -0,0 +1,32 @@
+/*
+//par ou impar
+function parimpar(n){
+    if(n%2==0){
+        return 'par!!'
+    }else {
+        return 'impar!!'
+    }
+}
+console.log(parimpar(1905))
+
+
+//somando dois numeros, se nao atribuir nenhum valor, o valor será 0
+function soma(n1=0,n2=0){
+    return n1+n2
+}
+console.log(soma(1000, 90))
+
+
+//fatorial de um numero (!)
+function fatorial(n){
+    let fat = 1
+    for(let c = n; c > 1; c--){
+        fat *= c
+    }
+    return fat
+}
+
+console.log(fatorial(56))
+
+
+*/
